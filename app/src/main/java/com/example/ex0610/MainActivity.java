c7e7c3a4dd37412413e6064f62be7f6026dbfb25
sh7478 <sh7478@bs.amalnet.k12.il>
2025-04-12 15:52:39 +0300

    End of code

--- app/src/main/java/com/example/ex0610/MainActivity.java
+++ app/src/main/java/com/example/ex0610/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.ex0610;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +16,36 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Switch switch1;
+    ToggleButton tb;
+
+    LinearLayout homepage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tb = findViewById(R.id.tb);
+        switch1 = findViewById(R.id.switch1);
+        homepage = findViewById(R.id.homePage);
+    }
+
+    public void change(View view)
+    {
+        if (tb.isChecked() && switch1.isChecked())
+        {
+            homepage.setBackgroundColor(Color.BLUE);
+        }
+        else if(tb.isChecked())
+        {
+            homepage.setBackgroundColor(Color.GREEN);
+        }
+        else if(switch1.isChecked())
+        {
+            homepage.setBackgroundColor(Color.YELLOW);
+        }
+        else
+        {
+            homepage.setBackgroundColor(Color.RED);
+        }
     }
 }
